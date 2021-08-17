76e7b83c89cecbd38eac5d5c4c19c7711f2166ba
papacheco <pedro.pacheco.a@gmail.com>
2021-08-17 09:07:54 -0300

    Renderizando main

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
  * instalar o wine (apt-get install wine) - Pode ser usado o Docker também
  */
 const {app, BrowserWindow, ipcMain} = require('electron');
-const path = require('path');
 const consultaml = require('./src/BackEnd/consultaml');
 let mainWindow;
 
@@ -15,9 +14,7 @@ function createWindow () {
     width: 600,
     height: 150,
     resizable:false,
-    //frame: false,
     webPreferences: {
-      //preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: false,
       enableRemoteModule: true,
@@ -48,22 +45,9 @@ ipcMain.on('canal1',(e,args)=>{
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+/*app.whenReady().then(() => {
   createWindow()
-  
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+})*/
+app.on('ready', () => {
+  createWindow()
 })
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
