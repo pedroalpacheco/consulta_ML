761160cafdf65a8b6c6712c6342bd97e31396221
papacheco <pedro.pacheco.a@gmail.com>
2021-08-18 10:25:33 -0300

    Criando modulo saveCsv

--- src/BackEnd/consultaml.js
+++ src/BackEnd/consultaml.js
@@ -5,14 +5,7 @@
  */
 
 const axios = require('axios');
-const ObjectsToCsv = require('objects-to-csv');
-const fs = require('fs');
-const {app, shell} = require('electron');
-
-//const diretorioraiz = app.getPath('documents');
-const diretorio = app.getPath('documents') +'/RELATORIOS-ML';
-//Pega horario da pesquisa
-const timestamp = new Date().getTime();
+const saveCsv = require('./saveCsv');
 
 const consultaml = (produto)=>{
   let verproduto = (!!produto);
@@ -21,21 +14,8 @@ const consultaml = (produto)=>{
   }else{
     //console.log(`Este é o produto digitado:${verproduto}`)
     const consulta = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
-    const nomeproduto = produto.replace(' ','');
-    const salvacsv = async (dados)=>{
-      const csv = new ObjectsToCsv(dados);
-      let arquivo = `${diretorio}/Relatorio-${nomeproduto}-${timestamp}.csv`
-         if (!fs.existsSync(diretorio)){
-          fs.mkdirSync(diretorio);
-          
-      }
-       
-        await csv.toDisk(arquivo);
-      setTimeout(function () {
-      shell.openPath(diretorio)
-  }, 3000);
-      
-    };
+    
+ 
     //Detalhe MLB = Brasil / MLB1459 = Imoveis
     axios.get(consulta)
       .then(function (response) {
@@ -55,7 +35,7 @@ const consultaml = (produto)=>{
         }else{
 
           console.log(verificaitens);//Debug
-          salvacsv(itens);
+          saveCsv(itens,produto);
           
         }
     
